16277ea5e9ab3b41404e07821ef595236455fc11
anacariac <acabreracariac@udesa.edu.ar>
2025-05-22 10:27:00 -0300

    cambi de carpetas y ejercicio 3

--- ejercicio_3/includes/DataGenerator.hpp
+++ ejercicio_3/includes/DataGenerator.hpp
@@ -12,5 +12,40 @@ private:
     vector<vector<int>> listas;
 
 public:
-    
+    template<typename T>
+    void addData(const T& data){
+        if constexpr(is_same_v<T, double>){
+            vec_doubles.push_back(data);
+        }
+        else if constexpr(is_same_v<T, string>){
+            palabras.push_back(data);
+        }
+        else if constexpr(is_same_v<T, vector<int>>){
+            listas.push_back(data);
+        }
+        else{
+            static_assert(!is_same_v<T, T>, "Tipo no aceptable");
+        }
+    }
+
+    template<typename T>
+    string getLabel(const T& data){
+        if constexpr(is_same_v<T, double>){
+            return "vec_doubles";
+        }
+        else if constexpr(is_same_v<T, string>){
+            return "palabras";
+        }
+        else if constexpr(is_same_v<T, vector<int>>){
+            return "listas";
+        }
+        else{
+            static_assert(!is_same_v<T, T>, "Tipo no aceptable");
+        }
+    }
+
+    //GETTERS
+    const vector<double>& getVecDoubles();
+    const vector<string>& getPalabras();
+    const vector<vector<int>>& getListas();
 };
